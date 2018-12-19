91c92482010f26ba74517f631f5b6afe97a03302
Julien Breux <julien.breux@gmail.com>
2018-12-19 22:17:04 +0100

    [var+docs] Remove unused part at this time and add example

--- internal/mila/var.go
+++ internal/mila/var.go
@@ -2,9 +2,5 @@ package mila
 
 // Var represents a variable
 type Var struct {
-	Default     string
-	Type        string
-	Required    bool
-	Description string
-	Value       string
+	Required bool
 }
