e123b3882071fb3933cae7c8debdb26ca64e6936
Julien Breux <julien.breux@gmail.com>
2018-12-19 22:13:20 +0100

    [checker] Add required validation to checker and prepare validators validation

--- internal/mila/checker/checker.go
+++ internal/mila/checker/checker.go
@@ -2,6 +2,8 @@ package checker
 
 import (
 	"errors"
+	"fmt"
+	"os"
 
 	"github.com/julienbreux/mila/internal/mila"
 )
@@ -9,6 +11,9 @@ import (
 var (
 	// ErrNoSchema if there is no schema to check
 	ErrNoSchema = errors.New("no schema")
+
+	// ErrNoVars if there are no variables
+	ErrNoVars = errors.New("no variable")
 )
 
 // Checker checks the schema according to the environment
@@ -17,7 +22,38 @@ func Checker(sc *mila.Schema) error {
 		return ErrNoSchema
 	}
 
-	// TODO: Do checker
+	if err := required(&sc.Vars); err != nil {
+		return err
+	}
+
+	if err := validated(&sc.Vars); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func required(vars *mila.Vars) error {
+	if vars == nil {
+		return ErrNoVars
+	}
+
+	for name, variable := range *vars {
+		_, ok := os.LookupEnv(name)
+		if variable.Required && !ok {
+			return fmt.Errorf("variable %s not found", name)
+		}
+	}
+
+	return nil
+}
+
+func validated(vars *mila.Vars) error {
+	if vars == nil {
+		return ErrNoVars
+	}
+
+	// TODO: Write validated core logic
 
 	return nil
 }
